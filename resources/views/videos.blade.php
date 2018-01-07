6ccf4a7830fdca76ddc0a1ee2a690cdbd772dc7a
Moshood Alaran <geekyshood@gmail.com>
2018-01-07 19:18:40 +0100

    fixed playmusic background image

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -19,8 +19,9 @@
 
 
       <section id="video-player">
-          <div class="container-fluid">
+          <div class="container">
               <div class="row">
+
               </div>
           </div>
       </section>
